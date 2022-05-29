0ab1a65867df5807756ab8c5ef835a959374ce30
kmaaallen@gmail.com <kmaaallen@gmail.com>
2022-05-29 17:51:53 +0100

    fix: Committing blog.js file ommitted in error

---
+++ src/blog.js
@@ -0,0 +1,10 @@
+export const blogs = [
+    {
+        title: 'My first blog',
+        published: '22/03/2022',
+        subtitle: 'Test blog post to check react components',
+        body: 'This is the body of my blog, here I want to write a lot. No formatting /n/n lets see if it reconises the new lines',
+        tags: 'CSS, theming',
+        references: '',
+    },
+]
