56ad4932688fc41286ac5d04de63f33e7897a89a
kmaaallen@gmail.com <kmaaallen@gmail.com>
2022-06-12 21:57:47 +0100

    feat: Added static folder to serve css and js files using express
    Exploring using MERN stack, adding express to serve files instead of previous.

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
-import { createServer } from 'http';
-import { readFile, readFileSync } from 'fs';
+import express from 'express';
+import * as url from 'url';
 
-createServer((req, res) => {
-    if (req.url === "/") {
-        readFile('src/index.html', (err, html) => {
-            if (err) {
-                throw err;
-            }
-            res.setHeader('Content-type', 'text/html');
-            res.write(html);
-            res.statusCode = 200;
-            res.end();
-        });
-    }
-    else if (req.url == '/styles/style.css') {
-        res.setHeader('Content-type', 'text/css');
-        res.write(readFileSync('styles/style.css'));
-        res.end();
-    } else if (req.url == '/index.js') {
-        res.setHeader('Content-type', 'text/javascript');
-        res.write(readFileSync('src/index.js'));
-        res.end();
-    } else {
-        res.write("invalid request")
-        res.end();
-    }
-}).listen(3000);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const app = express();
+
+app.use("/static", express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html');
+})
+
+
+app.listen(3000, function () {
+    console.log('listening on 3000');
+})
