81face8a13852a1deb551daec92b9172f69ec230
kmaaallen@gmail.com <kmaaallen@gmail.com>
2022-06-17 13:34:59 +0100

    feat: Restructured project and added React for MERN stack
    Reorganisation of file structure to have client and server directories.
    Adding React project in client directory instead of serving single index HTML.
    React components added to show theme buttons and display post snippets from db.
    Tests still included in server folder - to be re-vistited at later date.
    Github workflow updated.

---
+++ client/src/components/themeButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function ThemeButton(props) {
+
+    function selectTheme() {
+        document.getElementsByClassName('active-theme')[0].classList.remove('active-theme');
+        document.getElementById('body').classList.remove(...document.getElementById('body').classList);
+        document.getElementById('body').classList.add(props.id);
+        document.getElementById(props.id).classList.add('active-theme');
+        if (props.id === 'theme-light') {
+            document.getElementById('wcb').classList.remove('wcb-d');
+        } else {
+            document.getElementById('wcb').classList.add('wcb-d');
+        }
+    }
+
+    return (
+        <button id={props.id} onClick={selectTheme} className={props.active || ''}>
+            <i className={props.icon}></i> {props.label}
+        </button>
+    )
+}
--- server/server.js
+++ server/server.js
@@ -1,31 +1,20 @@
 const express = require('express');
-const url = require('url');
 const cors = require('cors');
-const path = require('path');
-const { connectToServer } = require('./db/dbConnect.js');
-const { routes } = require('./routes/routes.js');
+const port = process.env.PORT || 5000;
+const { connectToServer } = require('./db/dbConnect');
 
-//const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const app = express();
 
-//static files
-app.use('/static', express.static(path.join(__dirname, '..', 'static')));
 //cors
 app.use(cors({ origin: true, credentials: true }));
 //routes
-app.use(routes);
+app.use(require('./routes/routes'));
 
 // perform a database connection when the server starts
 connectToServer();
 
-// Get index page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'src', 'index.html'));
-});
-//green-project/src/index.html
-
 // start the Express server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
