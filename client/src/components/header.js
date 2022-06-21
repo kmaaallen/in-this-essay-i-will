0fcbef4c491d463ef4049712a550e4af26133f6e
kmaaallen@gmail.com <kmaaallen@gmail.com>
2022-06-21 08:43:34 +0100

    feat: Created header component
    Adding the header as a component allows me to move the theme buttons into the header. This way they will be available on all pages within the app for users.

---
+++ client/src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ThemeButton from './themeButton';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>The Green Project</h1>
+            <div>
+                <ThemeButton id='theme-light' label='Light Theme' icon='fa solid fa-sun color-accent' />
+                <ThemeButton id='theme-dark' label='Dark Theme' icon='fa solid fa-moon color-accent' active='active-theme' />
+            </div>
+        </header>
+
+    )
+}
