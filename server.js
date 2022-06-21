6fcff6713b10557ecb10789537f994687099a3ee
kmaaallen@gmail.com <kmaaallen@gmail.com>
2022-06-21 21:27:32 +0100

    fix: Added production serving of static files

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.use(cors({ origin: true, credentials: true }));
 //routes
 app.use(require('./routes/routes'));
 
+//production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 // perform a database connection when the server starts
 connectToServer();
 
