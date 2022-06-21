c6093f56297ea0ff2ee9c0795d18315791877b1f
kmaaallen@gmail.com <kmaaallen@gmail.com>
2022-06-21 23:07:47 +0100

    fix: Trying path resolve to serve static files

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(require('./routes/routes'));
 
 //production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.resolve(__dirname, "./client/build")));
 }
 
 // perform a database connection when the server starts
