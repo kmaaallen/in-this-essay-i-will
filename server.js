24ac6b6d54ab2a1d45b954e5cc28b117e38e045a
kmaaallen@gmail.com <kmaaallen@gmail.com>
2022-06-21 23:20:31 +0100

    fix: Changing position of production static code

--- server.js
+++ server.js
@@ -11,13 +11,17 @@ app.use(cors({ origin: true, credentials: true }));
 //routes
 app.use(require('./routes/routes'));
 
-//production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, "./client/build")));
-}
-
 // perform a database connection when the server starts
 connectToServer();
+
+//production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // start the Express server
 app.listen(port, () => {
