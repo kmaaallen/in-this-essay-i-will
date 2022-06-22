9ca846f20ed7b3fe9dc837b755f88a699e4d3528
kmaaallen@gmail.com <kmaaallen@gmail.com>
2022-06-22 09:01:05 +0100

    fix: updated server.js staticserve code

--- server.js
+++ server.js
@@ -11,16 +11,17 @@ app.use(cors({ origin: true, credentials: true }));
 //routes
 app.use(require('./routes/routes'));
 
-// perform a database connection when the server starts
-connectToServer();
-
 // serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-    //Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
 }
+
+// perform a database connection when the server starts
+connectToServer();
 
 // start the Express server
 app.listen(port, () => {
