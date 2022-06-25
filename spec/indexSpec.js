8df0f5f945c91b1959e986b6854d366e30661b48
kmaaallen@gmail.com <kmaaallen@gmail.com>
2022-06-25 11:26:21 +0100

    fix: Adding localhost:3000 as option if server url not found for CI testing

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config()
 
-const URL = `${process.env.REACT_APP_SERVER_URL}`;
+const URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3000';
 
 describe('Homepage', () => {
     var originalTimeout;
